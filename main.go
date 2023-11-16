876bf0c705811b3e0d6d07a359f4f4e10f073a9f
RageNeko26 <rneko2006@gmail.com>
2023-11-16 12:52:14 +0700

    feat: Add listener server

--- main.go
+++ main.go
@@ -1,9 +1,16 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-
+    io.WriteString(w, "Hello world \n")
   })
+
+  fmt.Println("Server is running")
+  http.ListenAndServe(":3000", nil)
 }
